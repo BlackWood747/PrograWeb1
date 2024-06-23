f4fcf3645dcbbdf2dd18dbd2aacc03b2c3a3b826
Juanzor <hjuan7217@gmail.com>
2024-06-23 18:53:37 -0300

    filtro arreglado

--- scripts/home.js
+++ scripts/home.js
@@ -6,6 +6,9 @@ const linkPeliculasBtn = document.querySelector('.linkPeliculas');
 const linkSeriesBtn = document.querySelector('.linkSeries');
 const linkHomeBtn = document.querySelector('.linkHome');
 
+let gen = 'todos';
+let cat = 'todos';
+
 const preguntarSiHayUsuario = () => {
     if (!localStorage.getItem('nombreDeUsuario')) {
         cerrarSesionBtn.style.display = 'none';
@@ -67,6 +70,146 @@ const filtrarPorCategoria = (cat) => {
     return listaFiltrada;
 };
 
+const filtrar = (cat, gen) => {
+    let listaFiltrada;
+    if (gen == 'todos' && cat == 'todos') {
+        return listaPeliculasSeries
+    } else if (gen == 'todos') {
+        listaFiltrada = listaPeliculasSeries.filter((i) => {
+            return i.categoria == cat;
+        });
+        console.log(listaFiltrada);
+    } else if (cat == 'todos') {
+        listaFiltrada = listaPeliculasSeries.filter((i) => {
+            return i.genero == gen;
+        });
+
+        console.log(listaFiltrada);
+        console.log(gen);
+        console.log(cat);
+    } else {
+        listaFiltrada = listaPeliculasSeries.filter((i) => {
+            return i.categoria == cat && i.genero == gen;
+        });
+        console.log(listaFiltrada);
+    }
+    console.log(listaFiltrada);
+
+    return listaFiltrada;
+};
+
+linkPeliculasBtn.addEventListener('click', (e) => {
+    cat ="pelicula"
+    let listaFiltrada = filtrar(cat, gen);
+    dibujarLista(listaFiltrada);
+});
+
+linkSeriesBtn.addEventListener('click', (e) => {
+    cat ="serie"
+
+    let listaFiltrada = filtrar(cat, gen);
+
+    dibujarLista(listaFiltrada);
+});
+
+linkHomeBtn.addEventListener('click', (e) => {
+    dibujarLista(listaPeliculasSeries);
+});
+
+selectGenero.addEventListener('change', (e) => {
+    gen = e.target.value;
+
+    
+
+    let listaFiltrada = filtrar(cat, gen);
+    dibujarLista(listaFiltrada);
+});
+
+//////////////
+
+/* 
+
+
+import { listaPeliculasSeries } from './dataPeliculas.js';
+('use strict');
+
+const cerrarSesionBtn = document.querySelector('.cerrarSesion');
+const linkPeliculasBtn = document.querySelector('.linkPeliculas');
+const linkSeriesBtn = document.querySelector('.linkSeries');
+const linkHomeBtn = document.querySelector('.linkHome');
+
+let genero = 'todos';
+let cat = 'todos';
+
+const preguntarSiHayUsuario = () => {
+    if (!localStorage.getItem('nombreDeUsuario')) {
+        cerrarSesionBtn.style.display = 'none';
+    } else {
+        cerrarSesionBtn.style.display = 'block';
+    }
+};
+
+cerrarSesionBtn.addEventListener('click', function () {
+    localStorage.clear();
+    preguntarSiHayUsuario();
+    console.log(localStorage.getItem('nombreDeUsuario'));
+});
+
+// Home => todas las peliculas y series!
+
+const layout = document.querySelector('.layout');
+
+const dibujarLista = (lista) => {
+    layout.innerHTML = '';
+    lista.forEach((i) => {
+        const cajaElement = document.createElement('div');
+        cajaElement.classList.add('caja');
+
+        const linkElement = document.createElement('a');
+        linkElement.href = `./detallesDePeliculas/${i.detalle}`;
+
+        const imgElement = document.createElement('img');
+        imgElement.src = i.img;
+        imgElement.alt = i.titulo;
+
+        linkElement.appendChild(imgElement);
+
+        cajaElement.appendChild(linkElement);
+
+        layout.appendChild(cajaElement);
+    });
+};
+dibujarLista(listaPeliculasSeries);
+
+const selectGenero = document.querySelector('.selectGenero');
+
+const filtrarPorGenero = (genero) => {
+    if (genero == 'todosLosGeneros') {
+        return listaPeliculasSeries;
+    } else {
+        let listaFiltrada = listaPeliculasSeries.filter((i) => {
+            return i.genero == genero;
+        });
+        console.log(listaFiltrada);
+        return listaFiltrada;
+    }
+};
+
+const filtrarPorCategoria = (cat) => {
+    let listaFiltrada = listaPeliculasSeries.filter((i) => {
+        return i.categoria == cat;
+    });
+    return listaFiltrada;
+};
+
+const filtrar = (cat, gen) => {
+    let listaFiltrada = listaPeliculasSeries.filter((i) => {
+        return i.categoria == cat && i.genero == gen;
+    });
+
+    return listaFiltrada;
+};
+
 linkPeliculasBtn.addEventListener('click', (e) => {
     let listaFiltrada = filtrarPorCategoria('pelicula');
     dibujarLista(listaFiltrada);
@@ -82,9 +225,9 @@ linkHomeBtn.addEventListener('click', (e) => {
 });
 
 selectGenero.addEventListener('change', (e) => {
-    let listaFiltrada = filtrarPorGenero(e.target.value);
-    console.log(listaFiltrada);
-    console.log(e.target.value);
-    dibujarLista(listaFiltrada);
+    genero = e.target.value;
 
+    let listaFiltrada = filtrarPorGenero(e.target.value);
+    dibujarLista(listaFiltrada);
 });
+ */
